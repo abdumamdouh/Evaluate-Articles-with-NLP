38e9b2270fb84756070ac4d2677b69b05769f3d0
abdumamdouh <eabdogfx@gmail.com>
2021-09-07 19:40:40 +0200

    handleFormSubmit logic

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,5 @@
 import handleFormSubmit from "./js/handleFormSubmit";
 
-/**
- * TODO: Get Value of the input for URL
- *  TODO: Check if it's URL or not
- *      yes
- *          send it to the backend
- *      no
- *          show user message it's not valid URL
- */
-
 import "./styles/resets.scss";
 import "./styles/base.scss";
 import "./styles/footer.scss";
@@ -21,5 +12,3 @@ import "./images/favicon.png";
 document
   .getElementById("main-form")
   .addEventListener("submit", (e) => handleFormSubmit(e));
-
-console.log("hhi");
--- src/client/js/handleFormSubmit.js
+++ src/client/js/handleFormSubmit.js
@@ -2,9 +2,47 @@ import checkURL from "./checkURL";
 
 const handleFormSubmit = (e) => {
   e.preventDefault();
-  console.log("hello");
-  const URL = document.getElementById("url-text");
+  // get the url input node
+  const urlInputElment = document.getElementById("url-text");
+  const URL = urlInputElment.value;
   console.log(URL);
+  //reset the input filed after submitting
+  urlInputElment.value = "";
+  // URL check
+  if (checkURL(URL)) {
+    try {
+      let submitBtnNode = document.getElementById("submitBtn");
+      let loggerNode = document.getElementById("loggerSection");
+      loggerNode.style.visibility = "visible";
+      submitBtnNode.disabled = true;
+      loggerNode.innerText = "Working...";
+    } catch (err) {
+      submitBtnNode.disabled = false;
+      console.log(err.message);
+      logger.style.visibility = "visible";
+      logger.innerText = "Error...";
+
+      // fetech request to the backend
+      const fetchAPI = await fetch(`http://localhost:8081/scan/${formText}`);
+      const APIResponse = await fetchAPI.json();
+      // add the results into the dom nodes
+      document.getElementById("agreement").innerHTML =
+        APIResponse.agreement.toLowerCase();
+      document.getElementById("subjectivity").innerHTML =
+        APIResponse.subjectivity.toLowerCase();
+      document.getElementById("confidence").innerHTML = APIResponse.confidence;
+      document.getElementById("irony").innerHTML =
+        APIResponse.irony.toLowerCase();
+
+      loggerNode.style.visibility = "hidden";
+      document.getElementById("sentiment-table").style.visibility = "visible";
+      submitBtnNode.disabled = false;
+    }
+  } else {
+    alert("Invalid URL, please make sure you are entering a valid URL.");
+    logger.innerText =
+      "Invalid URL, please try a working URL, ex: https://flaviocopes.com/npm-peer-dependencies/";
+  }
 };
 
 export default handleFormSubmit;
