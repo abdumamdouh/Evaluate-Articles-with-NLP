bdf4688e1e0386f78cc03b661a3fe71e71698506
abdumamdouh <eabdogfx@gmail.com>
2021-09-07 20:29:20 +0200

    add unit testing for checkURL function

--- _test_/testCheckURL.test.js
+++ _test_/testCheckURL.test.js
@@ -1,1 +1,15 @@
 import checkURL from "../src/client/js/checkURL";
+
+describe("Testing regex URL checking functionality", () => {
+  test("Making sure checkURL is defined", () => {
+    expect(checkURL).toBeDefined();
+  });
+
+  test("Making sure a valid URL returns true", () => {
+    expect(checkURL("www.facebook.com")).toBeTruthy();
+  });
+
+  test("Making sure an invalid URL returns false", () => {
+    expect(checkURL("Not A link")).toBeFalsy();
+  });
+});
