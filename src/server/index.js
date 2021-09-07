5a7021db19b45a1d84ceb85fff59185ce67c9aed
abdumamdouh <eabdogfx@gmail.com>
2021-09-07 21:59:52 +0200

    configure express server

--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,61 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const API_KEY = process.env.API_KEY;
+const API_KEY_CREDENTIALS = process.env.API_KEY;
 
-console.log(`Your API key is ${API_KEY}`);
+console.log(`Your API key Credentials is ${API_KEY_CREDENTIALS}`);
 
 const mockAPIResponse = require("./mockAPI.js");
 
-//import express
+//import express library
 
 const express = require("express");
 
-// create an instance for server
+// create an instance for server using express
 
 const app = express();
 
-// static folder
+// static folder selection
 app.use(express.static("dist"));
 // cors
 const cors = require("cors");
 app.use(cors());
 
-// import axios
+// import axios library
 
 const axios = require("axios");
 
 // select the port for the server
-const PORT = 8081;
+const PORT_NUMBER = 8081;
 
 // API endpoint
 
-app.listen(PORT, (err) => {
+app.get("/", (request, response) => {
+  response.sendFile("dist/index.html");
+});
+
+app.get("/analysis/*", async (request, response) => {
+  try {
+    const url = request.params[0];
+    // meaningcloud API URL var
+    const API_URL_LINK = "https://api.meaningcloud.com/sentiment-2.1";
+    // http get req to the API endpoint
+    const apiResponse = await axios.get(
+      `${API_URL_LINK}?key=${API_KEY_CREDENTIALS}&url=${url}&lang=en`
+    );
+    // get the api response and send it back tp the frontend
+    const { agreement, subjectivity, confidence, irony } = apiResponse.data;
+    response.send({ agreement, subjectivity, confidence, irony });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error..This shouldn't be happening..." + error);
+  }
+});
+
+app.listen(PORT_NUMBER, (err) => {
   if (err) {
-    throw new Error("err");
+    throw new Error("error!");
   } else {
-    console.log(`server is rinning on port ${PORT}`);
+    console.log(`server is running on port number ${PORT_NUMBER}!`);
   }
 });
