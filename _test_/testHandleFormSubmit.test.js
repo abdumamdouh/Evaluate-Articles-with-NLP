1034d647f470a087fa4f184f2b516b8769de214a
abdumamdouh <eabdogfx@gmail.com>
2021-09-07 20:31:56 +0200

    add unit testing for handleFormSubmit function

--- _test_/testHandleFormSubmit.test.js
+++ _test_/testHandleFormSubmit.test.js
@@ -1,1 +1,7 @@
 import handleFormSubmit from "../src/client/js/handleFormSubmit";
+
+describe("Testing the submit functionality of the form", () => {
+  test("Making sure handleFormSubmit is defined", () => {
+    expect(handleFormSubmit).toBeDefined();
+  });
+});
