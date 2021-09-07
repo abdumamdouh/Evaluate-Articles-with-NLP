37cf3442872e1ad5085e6e927a9c2109b488d8be
abdumamdouh <eabdogfx@gmail.com>
2021-09-07 03:26:52 +0200

    client side folder structure

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+// TODO: include your scss file here
+
+// TODO: get the button for submit
+// TODO: add event listener to it when the click to call handleSubmit function
+/**
+ * TODO: Get Value of the input for URL
+ *  TODO: Check if it's URL or not
+ *      yes
+ *          send it to the backend
+ *      no
+ *          show user message it's not valid URL
+ */
