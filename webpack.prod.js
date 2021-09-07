99a26e558d9274fdb5e8451d2052d4fa92b81ace
abdumamdouh <eabdogfx@gmail.com>
2021-09-07 02:30:57 +0200

    custom prod-config file for webpack

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-pkugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
